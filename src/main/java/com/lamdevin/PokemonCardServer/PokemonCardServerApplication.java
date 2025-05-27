d92e865cff8d2283d3952cb8a1142a239a11de25
Devin Lam <105399494+lamdevin@users.noreply.github.com>
2025-05-26 19:24:33 -0700

    Create Spring Boot project

---
+++ src/main/java/com/lamdevin/PokemonCardServer/PokemonCardServerApplication.java
@@ -0,0 +1,13 @@
+package com.lamdevin.PokemonCardServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokemonCardServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokemonCardServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lamdevin/PokemonCardServer/PokemonCardServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lamdevin.PokemonCardServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokemonCardServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
