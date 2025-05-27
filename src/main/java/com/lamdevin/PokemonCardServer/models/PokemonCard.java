0ce4ce7c5ff41fa80c6da069ad459c0cdc94321d
Devin Lam <105399494+lamdevin@users.noreply.github.com>
2025-05-26 19:31:45 -0700

    Add getters and setters for PokemonCard class

--- src/main/java/com/lamdevin/PokemonCardServer/models/PokemonCard.java
+++ src/main/java/com/lamdevin/PokemonCardServer/models/PokemonCard.java
@@ -11,4 +11,47 @@ public class PokemonCard {
     private String picture_url;
     private int hp;
 
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getRarity() {
+        return rarity;
+    }
+
+    public void setRarity(int rarity) {
+        this.rarity = rarity;
+    }
+
+    public String getPicture_url() {
+        return picture_url;
+    }
+
+    public void setPicture_url(String picture_url) {
+        this.picture_url = picture_url;
+    }
+
+    public int getHp() {
+        return hp;
+    }
+
+    public void setHp(int hp) {
+        this.hp = hp;
+    }
 }
