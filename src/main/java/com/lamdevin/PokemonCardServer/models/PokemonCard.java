37c6db27ce83dc60fb3de2f9adcf7f8beec24c8e
Devin Lam <105399494+lamdevin@users.noreply.github.com>
2025-05-26 19:30:13 -0700

    Create class for PokemonCard model

---
+++ src/main/java/com/lamdevin/PokemonCardServer/models/PokemonCard.java
@@ -0,0 +1,14 @@
+package com.lamdevin.PokemonCardServer.models;
+
+/**
+ * Represents a Pokemon Card, each with an ID, name, type, rarity, picture URL and HP (Health Points)
+ */
+public class PokemonCard {
+    private long id;
+    private String name;
+    private String type;
+    private int rarity;
+    private String picture_url;
+    private int hp;
+
+}
